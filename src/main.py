9f5d92cc6116de8596c966a7cbd1dd6e0e1f53ec
Sergio Estevao <smestevao11@gmail.com>
2022-05-28 16:31:29 +0100

    feat: added initial preprocessing

--- src/main.py
+++ src/main.py
@@ -4,14 +4,14 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as mpt
 import sklearn as sk
 import seaborn as sb
+from scipy import stats
 
 
 if __name__ == '__main__':
     dataset = pd.read_csv('data.csv', na_values=['NA'], delimiter=";")
 
-    dataset_corr = dataset.corr()
-    plt.figure(figsize=(20,20))
-    mask = np.zeros_like(dataset_corr)
-    mask[np.triu_indices_from(mask, k=1)] = True
-    sb.heatmap(dataset_corr, linewidths=0.1,cmap="YlGnBu", annot=True, square=True, mask=mask, fmt='.2f', annot_kws={"size": 6});
-    plt.show()
+    g = sb.FacetGrid(dataset, row="smoker", col="time", margin_titles=True)
+    g.map(dataset.scatterplot, "total_bill", "tip", alpha=.7)
+
+
+    
